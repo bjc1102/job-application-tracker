a042e044743232159b8af346d508886607facd78
byung ju choi <bjc30849@gmail.com>
2023-08-23 22:33:31 +0900

    add(server/db&orm): mysql Typeorm 연결

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as path from 'path';
 
 @Module({
   imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        retryAttempts: 10,
+        type: 'mysql',
+        host: 'localhost',
+        port: configService.get('MYSQL_PORT'),
+        database: 'job_application_tracker',
+        username: configService.get('MYSQL_USERNAME'),
+        password: configService.get('MYSQL_PASSWORD'),
+        entities: [path.join(__dirname, '/entities/**/*.entity.{js, ts}')],
+        synchronize: false,
+        logging: true,
+        timezone: 'local',
+      }),
+    }),
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath:
