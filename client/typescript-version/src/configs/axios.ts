fe4169dc2c717f5f8b07daaf2ad92bdc9d032b26
byung ju choi <bjc30849@gmail.com>
2023-08-25 21:58:30 +0900

    feat(client/axios): 401 error interceptors 설정

--- client/typescript-version/src/configs/axios.ts
+++ client/typescript-version/src/configs/axios.ts
@@ -1,7 +1,26 @@
 import axios from 'axios'
 
-// 인스턴스를 생성할때 config 기본값 설정하기
+const baseURL = 'http://localhost:5000'
+
 export const instance = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL,
   withCredentials: true // 서버로 쿠키를 보내려면 true로 설정
 })
+
+instance.interceptors.response.use(
+  response => response,
+  async error => {
+    if (error.response.status === 401) {
+      try {
+        const response = await axios.get(`${baseURL}/auth/refresh`, {
+          withCredentials: true
+        })
+        console.log(response)
+      } catch (refreshError) {
+        console.error('Token refresh failed', refreshError)
+      }
+    }
+
+    return Promise.reject(error)
+  }
+)
