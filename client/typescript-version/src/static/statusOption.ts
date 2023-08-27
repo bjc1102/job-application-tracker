4c98dc52832f7114a9a47031341e7b65f27584e2
byung ju choi <bjc30849@gmail.com>
2023-08-27 18:36:14 +0900

    feat(client/static): status option 추가

--- client/typescript-version/src/static/statusOption.ts
+++ client/typescript-version/src/static/statusOption.ts
@@ -3,14 +3,28 @@ export const statusOptions = [
   '',
   '서류지원',
   '서류합격',
+  '서류 + 코테합격',
   '서류불합격',
+  '서류 + 코테불합격',
+  '',
+  '코딩테스트합격',
+  '코딩테스트불합격',
   '',
   '과제테스트',
   '과제테스트합격',
   '과제테스트불합격',
   '',
-  '면접합격',
-  '면접불합격',
+  '1차면접',
+  '1차면접합격',
+  '1차면접불합격',
+  '',
+  '2차면접',
+  '2차면접합격',
+  '2차면접불합격',
+  '',
+  '최종면접',
+  '최종면접합격',
+  '최종면접불합격',
   '',
   '최종합격',
   '최종불합격'
@@ -19,6 +33,7 @@ export const statusOptions = [
 export const statusColorOption = (status: string) => {
   if (status.includes('불합격')) return 'warning'
   if (status.includes('합격')) return 'success'
+  if (status.includes('응답없음')) return 'default'
 
   return 'primary'
 }
