c46cfd36b3f9cb8a7a5639c309c6aea39476f64d
byung ju choi <bjc30849@gmail.com>
2023-09-04 22:21:49 +0900

    feat(client/dialog): confirm dialog 추가

---
+++ client/typescript-version/src/@core/components/alert-dialog/AlertDialog.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react'
+import Button from '@mui/material/Button'
+import Dialog from '@mui/material/Dialog'
+import DialogActions from '@mui/material/DialogActions'
+import DialogContent from '@mui/material/DialogContent'
+import DialogContentText from '@mui/material/DialogContentText'
+import DialogTitle from '@mui/material/DialogTitle'
+
+interface AlertDialogProps {
+  title: string
+  content: string
+  isOpen: boolean
+  handleAgree: () => void
+  handleDisagree: () => void
+}
+
+export default function AlertDialog({ title, content, isOpen, handleAgree, handleDisagree }: AlertDialogProps) {
+  return (
+    <Dialog open={isOpen} onClose={handleDisagree}>
+      <DialogTitle>{title}</DialogTitle>
+      <DialogContent>
+        <DialogContentText id='alert-dialog-description'>{content}</DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleAgree}>확인</Button>
+        <Button onClick={handleDisagree} autoFocus>
+          취소
+        </Button>
+      </DialogActions>
+    </Dialog>
+  )
+}
